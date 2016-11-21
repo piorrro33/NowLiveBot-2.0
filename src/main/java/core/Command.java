64e18b95c31fa14a54ba140d21035bd1f64200bb
Keith Russo <keesh@live.com>
2016-11-20 23:48:58 -0700

    Little bit of style cleanup

--- src/main/java/core/Command.java
+++ src/main/java/core/Command.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package core;
 
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
